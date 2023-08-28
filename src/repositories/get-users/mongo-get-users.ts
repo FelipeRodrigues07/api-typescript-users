b0c9b1e64caf6417bcfa17f397876b343bc81e74
Felipe Rodrigues <feliperodriguesrs07@outlook.com>
2023-08-28 17:48:20 -0300

    feat: add id to use

--- src/repositories/get-users/mongo-get-users.ts
+++ src/repositories/get-users/mongo-get-users.ts
@@ -1,15 +1,14 @@
 import { IGetUsersRepository } from "../../controllers/get-users/protocols";
+import { MongoClient } from "../../database/mongo";
 import { User } from "../../models/users";
 
 export class MongoGetUsersRepository implements IGetUsersRepository {
   async getUsers(): Promise<User[]> {
+  const users = await MongoClient.db.collection<User>('users').find({}).toArray() //vai pegar todos dados
+
+  users[0].
     return [
-      {
-        firstName: "Felipe",
-        lastName: "Rocha",
-        email: "felipe@gmail.com",
-        password: "123",
-      },
+        
     ];
   }
 } //quiser mudar o banco de dados
