776101fe22db8e26c015f5c7ae3e4ac48b05810c
Felipe Rodrigues <feliperodriguesrs07@outlook.com>
2023-08-29 16:07:21 -0300

    feat: add MongoUpdateUserRepository

--- src/controllers/update-user/protocols.ts
+++ src/controllers/update-user/protocols.ts
@@ -1,9 +1,14 @@
 import { User } from "../../models/users";
+import { HttpRequest, HttpResponse } from "../protocols";
 
 export interface UpdateUserParams {
   firstName?: string; //?como opcional pode so passar um
   lastName?: string;
   password?: string;
+}
+
+export interface IUpdateUserController {
+  handle(httpRequest: HttpRequest<any>): Promise<HttpResponse<User>>;
 }
 
 export interface IUpdateUserRepository {
